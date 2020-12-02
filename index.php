9cd973c93e887fc6221898baa01116ffb3daa7b2
RESHMA4887 <reshmavchandranpranavam@gmail.com>
2020-12-02 21:45:09 +0530

    asmnt1

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ASSIGHMENT</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+   <div class="bck">
+       <div class="row">
+           <div class="col">
+               <table class="table">
+                   <tr>
+                       <td>First Name:</td>
+                       <td><input type="text" class="form-control"></td>
+                       <td>Last Name:</td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td>Age</td>
+                       <td><input type="text" class="form-control"></td>
+                       <td>D.O.B</td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td>Mobile:</td>
+                       <td><input type="text" class="form-control"></td>
+                       <td>Email.id</td>
+                       <td><input type="text" class="form-control"></td>
+                   </tr>
+                   <tr>
+                       <td>Username:</td>
+                       <td><input type="text" class="form-control"></td>
+                       <td>Password:</td>
+                       <td><input type="password" class="form-control"></td>
+
+                       
+                   </tr>
+                   <tr>
+                       <td>Confirm password</td>
+                       <td><input type="password" class="form-control"></td>
+                       <td></td>
+                       <td><button class="btn-danger">OK</button></td>
+                   </tr>
+               </table>
+           </div>
+       </div>
+   </div>
+</body>
+</html>
